c687b161e6fd5593914b41253d0762394e775fd8
Nyland Sidifall <npsidifall@aggies.ncat.edu>
2020-06-17 22:24:04 -0400

    Added list view to trips
    Added a horizontal list view to trips pages

--- flutter_rental_app/lib/Screens/trips_page.dart
+++ flutter_rental_app/lib/Screens/trips_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutterrentalapp/Models/AppConstants.dart';
+import 'package:flutterrentalapp/Views/grid_widgets.dart';
 import 'package:flutterrentalapp/Views/text_widgets.dart';
 
 
@@ -34,6 +35,20 @@ class _trips_page_state extends State<trips_page> {
               padding: const EdgeInsets.only(top:15, bottom:25),
               child: Container(
                 height: MediaQuery.of(context).size.height/3,
+                child: ListView.builder(
+                  itemCount: 3,
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index){
+                    return Padding(
+                      padding: const EdgeInsets.only(right:15.0),
+                      child: Container(
+                          width: MediaQuery.of(context).size.width/2.5,
+                          child: trip_grid_tile()
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
             Text(
@@ -47,6 +62,20 @@ class _trips_page_state extends State<trips_page> {
               padding: const EdgeInsets.only(top:15, bottom:25),
               child: Container(
                 height: MediaQuery.of(context).size.height/3,
+                child: ListView.builder(
+                  itemCount: 2,
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  itemBuilder: (context, index){
+                    return Padding(
+                      padding: const EdgeInsets.only(right:15.0),
+                      child: Container(
+                          width: MediaQuery.of(context).size.width/2.5,
+                          child: trip_grid_tile()
+                      ),
+                    );
+                  },
+                ),
               ),
             ),
           ],
--- flutter_rental_app/lib/Views/grid_widgets.dart
+++ flutter_rental_app/lib/Views/grid_widgets.dart
@@ -65,3 +65,68 @@ class _posting_grid_tile_state extends State<posting_grid_tile> {
     );
   }
 }
+
+class trip_grid_tile extends StatefulWidget {
+  trip_grid_tile({Key key}) : super(key: key);
+
+  @override
+  _trip_grid_tile_state createState() => _trip_grid_tile_state();
+}
+
+class _trip_grid_tile_state extends State<trip_grid_tile> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        AspectRatio(
+          aspectRatio: 3 / 2
+          ,
+          child: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/apartment.jpg'),
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
+        ),
+
+        AutoSizeText(
+          'Awesome Apartment',
+          style: TextStyle(
+            fontSize: 17,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        AutoSizeText(
+          'Vancouver, BC',
+          style: TextStyle(
+            fontSize: 16,
+          ),
+        ),
+        Text(
+          '\$120 / night',
+          style: TextStyle(
+            fontSize: 10,
+          ),
+        ),
+        Text(
+          'August 10th, 2019',
+          style: TextStyle(
+            fontSize: 10,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          'August 12th, 2019',
+          style: TextStyle(
+            fontSize: 10,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
+  }
+}
