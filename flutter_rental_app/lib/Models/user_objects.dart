c48a77a1d60f278610e78be0788de706a3a1ad16
Nyland Sidifall <npsidifall@aggies.ncat.edu>
2020-06-23 18:49:58 -0400

    Create user_objects.dart
    started work on building data structres for app. this is contact and posting class.

---
+++ flutter_rental_app/lib/Models/user_objects.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Contact{
+  String firstName;
+  String lastName;
+  String fullName;
+  String imagePath;
+  AssetImage displayImage;
+
+  Contact({this.firstName = "", this.lastName="", this.imagePath=""}){
+    this.fullName = this.firstName +" "+this.lastName;
+    this.displayImage = AssetImage(this.imagePath);
+  }
+
+  User createUserFromContact(){
+    return User(
+      firstName: this.firstName,
+      lastName: this.lastName,
+      imagePath: this.imagePath,
+    );
+  }
+
+}
+
+
+class User extends Contact{
+  String email;
+  String bio;
+  String city;
+  String country;
+  bool isHost;
+  bool isCurrentlyHosting;
+
+  User({String firstName = "", String lastName="", String imagePath="",
+  this.email = "", this.bio = "", this.city = "", this.country=""
+  }):super(firstName: firstName, lastName: lastName, imagePath: imagePath){
+    this.isHost = false;
+    this.isCurrentlyHosting = false;
+  }
+
+  void changeCurrentlyHosting(bool isHosting){
+    this.isCurrentlyHosting = isHosting;
+  }
+
+  void becomeHost(){
+    this.isHost = true;
+    this.changeCurrentlyHosting(true);
+  }
+
+  Contact createContactFromUser(){
+    return Contact(
+      firstName: this.firstName,
+      lastName: this.lastName,
+      imagePath: this.imagePath
+    );
+  }
+
+}
